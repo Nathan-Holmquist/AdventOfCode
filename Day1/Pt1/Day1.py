43a4e3d0f8d461db9d370cd4068bb64d050e00a1
Nathan Holmquist <59553145+Nathan-Holmquist@users.noreply.github.com>
2024-12-01 18:00:32 -0500

    Day 1 Part 1 Complete

---
+++ Day1/Pt1/Day1.py
@@ -0,0 +1,26 @@
+
+column1 = []
+column2 = []
+
+with open ("input.txt") as file:
+    lines = file.readlines()
+    for line in lines:
+        data = line.split()
+        column1.append(int(data[0]))
+        column2.append(int(data[1]))
+
+
+column1.sort()
+column2.sort()
+
+total_distance = 0
+for i in range(len(column1)):
+    if column1[i] > column2[i]:
+        total_distance += column1[i] - column2[i]
+    elif column1[i] < column2[i]:
+        total_distance += column2[i] - column1[i]
+    else:
+        total_distance += 0
+
+print("Total Distance:", total_distance)
+
